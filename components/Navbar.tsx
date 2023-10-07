82a7bb17c7609aea5aa554f96612a40b5808bd0a
mariak-fla <mariak.fla@gmail.com>
2023-10-07 00:54:30 -0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,3 @@
-import { Info } from "lucide-react"
 import { CustomButton, GameRules, DarkLightModeToggle } from "./"
 import { useState } from "react"
 
@@ -10,9 +9,8 @@ function Navbar() {
       <div className="fixed top-0 w-full flex justify-between px-10 py-4 text-md items-center border-b dark:border-white/50 border-black/25 backdrop-blur-md bg-white/5 dark:bg-black/5">
         <CustomButton
           title="Como jogar?"
-          containerStyles="bg-amber-600 hover:bg-amber-700 duration-200 px-5 py-.75 rounded-full"
+          containerStyles="bg-amber-600 hover:bg-amber-700 duration-200 px-5 py-1 rounded-full"
           textStyles="gap-2 text-slate-200"
-          rightIcon={<Info size={16} className="text-slate-200" />}
           handleClick={() => setIsOpen(true)}
         />
 
